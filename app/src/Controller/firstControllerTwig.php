410b9270123aae1a839d1c5dcf930157069935e0
Rebas <reah24@bth.se>
2024-03-31 16:35:19 +0200

    add symfony

---
+++ app/src/Controller/firstControllerTwig.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class firstControllerTwig extends AbstractController
+{
+
+    #[Route("/home", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+
+}

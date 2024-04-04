5337506e598cad9b3f4ea2c70855daaf06d10d85
Rebas <reah24@bth.se>
2024-04-04 02:56:48 +0200

    add json page

---
+++ app/src/Controller/apiData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class apiData
+{
+    #[Route("/api/quote")]
+    public function jsonNumber(): Response
+    {
+        date_default_timezone_set('Europe/Stockholm');
+   
+        $quotes = [
+            "Jag är stark idag!!",
+            "Jag är redo idag!!",
+            "Jag är effektiv idag!!"
+        ];
+    
+        $randomQuote = $quotes[array_rand($quotes)];
+    
+
+        $todayDate = date("Y-m-d");
+    
+     
+        $timestamp = time();
+  
+        $time = date("H:i:s", $timestamp);
+    
+        $data = [
+            'quote' => $randomQuote,
+            'date' => $todayDate,
+            'time' => $time
+        ];
+    
+        // Return the JSON response
+        return new JsonResponse($data);
+    }
+
+}
--- app/src/Controller/firstControllerTwig.php
+++ app/src/Controller/firstControllerTwig.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class firstControllerTwig extends AbstractController
 {
 
-    #[Route("/home", name: "home")]
+    #[Route("/", name: "home")]
     public function home(): Response
     {
         return $this->render('home.html.twig');

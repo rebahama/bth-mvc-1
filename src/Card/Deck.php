0c5d38e4f0f6f12fee22e715c74f0860fc2bd80f
Rebas <reah24@bth.se>
2024-04-25 01:51:06 +0200

    add code cleaner

---
+++ src/Card/Deck.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Card;
+
+class Deck
+{
+    public $cards = [];
+
+    public function __construct()
+    {
+        $suits = ['♥', '♠', '♦', '♣']; // Hearts, Spades, Diamonds, Clubs
+        $values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK:🂫', 'QUEEN:🂭', 'KING:🂮', 'ACE:🃑']; // Larger representations of Jack, Queen, King, and Ace
+
+
+        foreach ($suits as $suit) {
+            foreach ($values as $value) {
+                $this->cards[] = new Card($suit, $value);
+            }
+        }
+    }
+
+    public function sortBySuitAndValue()
+    {
+        usort($this->cards, function ($a, $b) {
+            // Define the order of card values
+            $valueOrder = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK:🂫', 'QUEEN:🂭', 'KING:🂮', 'ACE:🃑'];
+            $aIndex = array_search($a->value, $valueOrder);
+            $bIndex = array_search($b->value, $valueOrder);
+
+            // If both cards have the same suit, compare their values
+            if ($a->suit == $b->suit) {
+                return $aIndex - $bIndex; // Compare based on the order index
+            }
+
+            // If the suits are different, compare their suits
+            return strcmp($a->suit, $b->suit);
+        });
+    }
+
+    public function randomCard()
+    {
+        shuffle($this->cards);
+    }
+
+    public function drawCard()
+    {
+        return array_shift($this->cards);
+    }
+
+    public function getNumberOfCardsLeft()
+    {
+        return count($this->cards);
+    }
+}
---
+++ src/Controller/firstControllerTwig.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class firstControllerTwig extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+    #[Route("/lucky", name: "lucky")]
+    public function lucky(): Response
+    {
+        $number = random_int(0, 100);
+
+        // Determine the image URL based on the value of $number
+        if ($number > 50) {
+            $imageUrl = 'background.jpg';
+        } else {
+            $imageUrl = 'symfony.jpg';
+        }
+
+        // Add the image URL to the $data array
+        $data = [
+            'number' => $number,
+            'imageUrl' => $imageUrl
+        ];
+
+        return $this->render('lucky_number.html.twig', $data);
+    }
+
+    #[Route("/api", name: "api")]
+    public function jsonIndex(): Response
+    {
+        return $this->render('api.html.twig');
+    }
+
+
+}

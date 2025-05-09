a42fce9615d0dfff073f5e5d04eef2d3f62eda38
Rebas <reah24@bth.se>
2025-05-09 03:19:09 +0200

    init amd create and add library with properites

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Library;
+use Doctrine\Persistence\ManagerRegistry;
+
+final class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    #[Route('/library/create', name: 'library_create')]
+    public function createLibrary(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $library = new Library();
+        $library->setTitle('Sample Book ' . rand(1, 100));
+        $library->setIsbn('978-3-' . rand(1000000, 9999999));
+        $library->setAuthor('Author ' . rand(1, 5));
+        $library->setImagePath('uploads/sample.jpg');
+
+        $entityManager->persist($library);
+        $entityManager->flush();
+
+        return new Response('Saved new library entry with id ' . $library->getId());
+    }
+}

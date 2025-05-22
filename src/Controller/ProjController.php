7915b26395c2c5746fff5e7e8bf387d9052483fa
Rebas <reah24@bth.se>
2025-05-22 02:51:46 +0200

    add brand and about page

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -13,7 +13,17 @@ class ProjController extends AbstractController
     {
         return $this->render('proj/main-page.html.twig');
     }
-   
 
+    #[Route("/proj/about", name: "proj_about")]
+    public function projAbout(): Response
+    {
+        return $this->render('proj/proj-about.html.twig');
+    }
+
+    #[Route("/proj/brands", name: "proj_brands")]
+    public function projBrands(): Response
+    {
+        return $this->render('proj/brands-page.html.twig');
+    }
 
 }
